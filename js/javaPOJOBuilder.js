a1391e5e2f2bb3b715ae7b254ee740b03c8ef6c1
Joseph Mate <mate.joseph@gmail.com>
2018-01-13 18:05:37 -0500

    refactored

--- js/javaPOJOBuilder.js
+++ js/javaPOJOBuilder.js
@@ -9,64 +9,89 @@ function Field(name, isFinal, type) {
 	this.type = type;
 }
 
-var indent = "    ";
 var data = new Data();
 
 function capaitalizeFirstLetter(str) {
 	return str.charAt(0).toUpperCase() + str.slice(1);
 }
 
+function indent(numOfIndents) {
+	var output = "";
+	for(var i = 0; i < numOfIndents; i++) {
+		output += "    ";
+	}
+	return output;
+}
+
+function classHeader(className, indentSize) {
+	return indent(indentSize) + "public class " + className + " {\n\n";
+}
+
+function fieldStr(field, indentSize) {
+	var output = indent(indentSize) + "private ";
+	if(field.isFinal) {
+		output += "final ";
+	}
+	output += field.type + " " + field.name + ";\n\n";
+	return output
+}
+
+function getterFcn(field, indentSize) {
+	var output = "";
+	output += indent(indentSize) + "public " + field.type + " get" + capaitalizeFirstLetter(field.name) + "() {\n";
+	output += indent(indentSize + 1) + "return " + field.name + ";\n";
+	output += indent(indentSize) + "}\n\n";
+	return output;
+}
+
+function withFcn(field, indentSize) {
+	var output = "";
+	output += indent(2) + "public Builder with" + capaitalizeFirstLetter(field.name) + "(" + field.type + " " + field.name + ") {\n";
+	output += indent(3) + "this." + field.name + " = " + field.name + ";\n";
+	output += indent(3) + "return this;\n";
+	output += indent(2) + "}\n\n";
+	return output;
+}
+
 function generatePOJO() {
-	var output = "public class " + data.className + " {\n\n";
+	var output = classHeader(data.className);
 
 	for(var fieldIdx in data.fields) {
-		var field = data.fields[fieldIdx];
-		output += indent + "private ";
-		if(field.isFinal) {
-			output += "final ";
-		}
-		output += field.type + " " + field.name + ";\n\n";
+		output += fieldStr(data.fields[fieldIdx], 1);
 	}
 
 	// Constructor
-	output += indent + "private " + data.className + "(Builder builder) {\n";
+	output += indent(1) + "private " + data.className + "(Builder builder) {\n";
 	for(var fieldIdx in data.fields) {
 		var field = data.fields[fieldIdx];
-		output += indent + indent + "this." + field.name + " = builder." + field.name + ";\n";
+		output += indent(2) + "this." + field.name + " = builder." + field.name + ";\n";
 	}
-	output += indent + "}\n\n";
+	output += indent(1) + "}\n\n";
 
 	// Getters
 	for(var fieldIdx in data.fields) {
-		var field = data.fields[fieldIdx];
-		output += indent + "public " + field.type + " get" + capaitalizeFirstLetter(field.name) + "() {\n";
-		output += indent + indent + "return " + field.name + ";\n";
-		output += indent + "}\n\n";
+		output += getterFcn(data.fields[fieldIdx], 1);
 	}
 
 	// Builder
-	output += indent + "public class Builder {\n\n";
+	output += classHeader("Builder", 1);
 
 	// Builder's fields
 	for(var fieldIdx in data.fields) {
 		var field = data.fields[fieldIdx];
-		output += indent + indent + "private " + field.type + " " + field.name + ";\n\n";
+		output += fieldStr(new Field(field.name, false, field.type), 2);
 	}
 
 	// Builder Setters
 	for(var fieldIdx in data.fields) {
-		var field = data.fields[fieldIdx];
-		output += indent + indent + "public Builder with" + capaitalizeFirstLetter(field.name) + "(" + field.type + " " + field.name + ") {\n";
-		output += indent + indent + indent + "this." + field.name + " = " + field.name + ";\n";
-		output += indent + indent + indent + "return this;\n";
-		output += indent + indent + "}\n\n";
+		output += withFcn(data.fields[fieldIdx]);
 	}
 
 	// End Builder
-	output += indent + indent + "public " + data.className + " build() {\n";
-	output += indent + indent + indent + "return new " + data.className + "(this);\n";
-	output += indent + indent + "}\n\n";
-	output += indent + "}\n\n";
+	output += indent(2) + "public " + data.className + " build() {\n";
+	output += indent(3) + "return new " + data.className + "(this);\n";
+	output += indent(2) + "}\n\n";
+	output += indent(1) + "}\n\n";
 
 	// End Class
 	output += "}\n";
